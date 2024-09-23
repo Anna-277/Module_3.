9d60dc44cadc24347234c679b3a539cb20caa937
Anna-277 <yuava@inbox.ru>
2024-09-23 12:22:31 +0500

    ДЗ Распаковка позиционных параметров" задача "Распаковка"

---
+++ Module_3.3.py
@@ -0,0 +1,15 @@
+# Функция с параметрами по умолчанию:
+def print_params(a = 1, b = 'строка', c = True):
+    print(print_params)
+    return
+
+
+print_params()
+print_params(1, "строка", True)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+# Распаковка параметров:
+values_list = [17, "мухомор", True]
+values_dict = {print_params()}
+print(values_dict)
