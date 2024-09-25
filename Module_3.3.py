782f82e74111cdc66d94c3aee2c246646f3ca561
Anna-277 <yuava@inbox.ru>
2024-09-25 23:06:44 +0500

    ДЗ Распаковка позиционных параметров" задача "Распаковка"

--- Module_3.3.py
+++ Module_3.3.py
@@ -1,16 +1,26 @@
-# Функция с параметрами по умолчанию:
-def print_params(a = 1, b = 'строка', c = True):
-    a = str(a)
-    print(print_params)
-    return
-
+# 1. Функция с параметрами по умолчанию:
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
 
 print_params()
 print_params(1, "строка", True)
-print_params(b = 25)
-print_params(c = [1,2,3])
+print_params(b=25)
+print_params(c=[1, 2, 3])
 
-# Распаковка параметров:
+# 2. Распаковка параметров:
+def function_print_key_value(**my_dict):
+    for key, value in my_dict.items():  # проходим ключом и значением по распакованному словарю
+        print(f'key: {key} value: {value}')
+
 values_list = [17, "мухомор", True]
-values_dict = {print_params()}
-print(values_dict)
+values_dict = {"a": 1, "b": 2, "c": 3}
+
+function_print_key_value(**values_dict)
+
+print_params(*values_list) # передаем распакованный список в функцию
+print_params(**values_dict) # передаем распакованный словарь
+
+
+# 3. Распаковка + отдельные параметры:
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
